b498b4f7d9f56b989eeba2320ef44180a5114df9
Zaire Ali <zaireali649@gmail.com>
2023-07-31 23:14:54 +0000

    Cities

---
+++ cities.py
@@ -0,0 +1,10 @@
+city_1 = "New York City"
+city_2 = "Los Angeles"
+city_3 = "Chicago"
+city_4 = "Houston"
+city_5 = "Philadelphia"
+
+empty_list = []
+top_cities = ["New York City", "Los Angeles", "Chicago", "Houston", "Philadelphia"]
+print(top_cities)
+print(top_cities[0])
